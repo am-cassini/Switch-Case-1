4626a25ce83cd72075ab2934348322b16500827a
am-cassini <75113126+am-cassini@users.noreply.github.com>
2021-10-15 14:58:58 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// ITN114 Wireless Internet and Pervasive Computing
+// Switch case C# exercise 1
+// Azalea Mae C. Manimog
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter time of day (Day/Night):");
+            String input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "D":
+                    Console.WriteLine("Day");
+            break;
+                case "N":
+                    Console.WriteLine("Night");
+            break;
+            }
+
+        }
+
+    }
+}
